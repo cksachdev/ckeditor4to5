83461e153bdeefd23aebfb06dad668a50f56867d
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2019-11-07 16:46:07 +0530

    Added easyimage plugin to support image alignment

--- src/app/ckeditor-tool/ckeditor-tool.component.ts
+++ src/app/ckeditor-tool/ckeditor-tool.component.ts
@@ -84,17 +84,47 @@ export class CkeditorToolComponent implements OnInit {
       'plugin.js'
     );
 
+    CKEDITOR.addCss('figure[class*=easyimage-gradient]::before { content: ""; position: absolute; top: 0; bottom: 0; left: 0; right: 0; }' +
+      'figure[class*=easyimage-gradient] figcaption { position: relative; z-index: 2; }' +
+      '.easyimage-gradient-1::before { background-image: linear-gradient( 135deg, rgba( 115, 110, 254, 0 ) 0%, rgba( 66, 174, 234, .72 ) 100% ); }' +
+      '.easyimage-gradient-2::before { background-image: linear-gradient( 135deg, rgba( 115, 110, 254, 0 ) 0%, rgba( 228, 66, 234, .72 ) 100% ); }');
+
     CKEDITOR.replace('editor1', {
-      extraPlugins: 'ckeditor_wiris',
+      extraPlugins: ['ckeditor_wiris', 'easyimage'],
       // For now, MathType is incompatible with CKEditor file upload plugins.
-      removePlugins:
-        'uploadimage,uploadwidget,uploadfile,filetools,filebrowser',
-      height: 320,
+      removePlugins: 'uploadimage,uploadwidget,uploadfile,image',
+      height: 520,
       // Update the ACF configuration with MathML syntax.
-      extraAllowedContent:
-        this.mathElements.join(' ') +
-        '(*)[*]{*};img[data-mathml,data-custom-editor,role](Wirisformula)'
+      extraAllowedContent: this.mathElements.join(' ') + '(*)[*]{*};img[data-mathml,data-custom-editor,role](Wirisformula)',
+      removeDialogTabs: 'image:advanced;link:advanced',
+      easyimage_toolbar: [ 'EasyImageAlignLeft', 'EasyImageAlignCenter', 'EasyImageAlignRight' ],
+      toolbar: [{
+          name: 'document',
+          items: ['Undo', 'Redo']
+        },
+        {
+          name: 'styles',
+          items: ['Format']
+        },
+        {
+          name: 'basicstyles',
+          items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat']
+        },
+        {
+          name: 'paragraph',
+          items: ['NumberedList', 'BulletedList']
+        },
+        {
+          name: 'links',
+          items: ['Link', 'Unlink']
+        },
+        {
+          name: 'wiris',
+          items : [ 'ckeditor_wiris_formulaEditor', 'ckeditor_wiris_formulaEditorChemistry']
+        }
+      ],
     });
+
     this.model.editorData = CKINPUT.result.assessment_item.question;
   }
 }
